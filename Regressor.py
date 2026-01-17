c7b928f4f1afea8a2009af1e58865c50cb2b12e3
ShubhamGurjarIITBOMBAY <99013446+ShubhamGurjarIITBOMBAY@users.noreply.github.com>
2026-01-17 16:58:49 +0530

    added the linear algebra version

--- Regressor.py
+++ Regressor.py
@@ -5,10 +5,31 @@ import data_utils as du
 
 def regression(file):
     df=du.file_reader(file)
-    if df.shape[1]==2:
-        b1=np.sum((df[:0]-np.mean(df[:0]))@(df[:1]-np.mean(df[:1])))/np.sum((df[:0]-np.mean(df[:0]))@(df[:0]-np.mean(df[:0])))
-        b0=np.mean(df[:1])-b1*np.mean(df[:0])
-        return b1,b0
+    
+    if df.shape[1] == 2:
+        x = df[:, 0]
+        y = df[:, 1]
+
+        x_mean = np.mean(x)
+        y_mean = np.mean(y)
+
+        b1 = np.sum((x - x_mean) * (y - y_mean)) / np.sum((x - x_mean) ** 2)
+        b0 = y_mean - b1 * x_mean
+
+        return b1, b0
+
+    else:
+
+        X = df[:, :-1]   
+        y = df[:, -1]    
+
+        ones = np.ones((X.shape[0], 1))
+        X = np.hstack((ones, X))
+
+        beta = np.linalg.inv(X.T @ X) @ X.T @ y
+
+        return beta
+
     
 
 
